b5be32b271889aca8cf4155d835c2443df1902d2
dilia <dilia@e[ample.com>
2023-08-14 12:28:00 +0600

    august

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.scss";
 import Header from "./components/Header";
-import Course from "./components/Course";
 import { Route, Routes } from "react-router";
 import AboutUs from "./components/aboutUs/aboutUs";
 import Main from "./components/Main/index";
@@ -8,6 +7,11 @@ import Footer from "./components/Footer/Footer";
 import Reg from "./components/reg/Reg";
 import ZapylParol from "./components/reg/ZabylParol";
 import AboutCourse from './components/AboutCourse'
+import Marketing1 from "./components/Marceting1";
+import Lessons from "./components/Lessons";
+
+
+
 function App() {
   return (
     <div className="App">
@@ -18,7 +22,9 @@ function App() {
         <Route path="/aboutUs" element={<AboutUs />} />
         <Route path="/zapylParol" element={<ZapylParol/>} />
         <Route path="/aboutCourse" element={<AboutCourse/>} />
+        <Route path="/marketing1" element={<Marketing1/>} />
       </Routes>
+     
       <Footer/>
     </div>
   );
--- src/components/AboutCourse/index.js
+++ src/components/AboutCourse/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import man from '../../img/young man working on a laptop.png'
+import lesson1 from '../../img/lesson1.png'
+import lesson2 from '../../img/lesson2.png'
+import lesson3 from '../../img/lesson3.png'
+import lesson4 from '../../img/lessson4.png'
+import lesson5 from '../../img/lesson5.png'
+import lesson6 from '../../img/lesson6.png'
+import { RiAlarmLine} from 'react-icons/ri'
+import  {RiArrowRightSLine} from 'react-icons/ri'
+import { NavLink } from 'react-router-dom';
+import Lessons from '../Lessons';
+
 
 const AboutCourse = () => {
     return (
-        <div id='abCourse'>
+       <>
+           <div id='abCourse'>
             <div className="container">
                 <div className="abCourse">
                     <div>
@@ -29,9 +41,223 @@ const AboutCourse = () => {
                         <button className='btn-2' >Продажи</button>
                     </div>
                 </div>
+               
+          
+                <div className="lessons">
+                <div className='first'>
+                <div className='top'>
+                           <img className='top-img' src={lesson1}></img>
+                            <div className='top-text'>
+                            <h4>Как ставить о оценивать задачи</h4>
+                            <p className='top-p' >   Мы ориентируемся на эргономику и ты где работаешь. Это всего лишь нажатие клавиши.</p>
+                            <div className='topic1' >
+                                        <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                         <p>22ч 30мин</p>
+                               
+                                         <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                    <p>22ч 30мин</p>
+                              
+                               
+                                    <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                    <p>22ч 30мин</p>
+                               
+                                
+                            </div> 
+                          <button className='top-btn'>Узнать больше <RiArrowRightSLine style={{
+                                 width: '30px' ,
+                                 height: '20px'
+                            }} /> </button> 
+                              </div>
+                        </div>
+                        <div className='top'>
+                           <img className='top-img' src={lesson2}></img>
+                            <div className='top-text'>
+                            <h4>Как ставить о оценивать задачи</h4>
+                            <p className='top-p' >   Мы ориентируемся на эргономику и ты где работаешь. Это всего лишь нажатие клавиши.</p>
+                            <div className='topic1' >
+                                        <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                         <p>22ч 30мин</p>
+                               
+                                         <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                    <p>22ч 30мин</p>
+                              
+                               
+                                    <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                    <p>22ч 30мин</p>
+                               
+                                
+                            </div> 
+                          <button className='top-btn'>Узнать больше <RiArrowRightSLine style={{
+                                 width: '30px' ,
+                                 height: '20px'
+                            }} /> </button> 
+                              </div>
+                        </div>
+                        <div className='top'>
+                           <img className='top-img' src={lesson3}></img>
+                            <div className='top-text'>
+                            <h4>Как ставить о оценивать задачи</h4>
+                            <p className='top-p' >   Мы ориентируемся на эргономику и ты где работаешь. Это всего лишь нажатие клавиши.</p>
+                            <div className='topic1' >
+                                        <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                         <p>22ч 30мин</p>
+                               
+                                         <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                    <p>22ч 30мин</p>
+                              
+                               
+                                    <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                    <p>22ч 30мин</p>
+                               
+                                
+                            </div> 
+                          <button className='top-btn'>Узнать больше <RiArrowRightSLine style={{
+                                 width: '30px' ,
+                                 height: '20px'
+                            }} /> </button> 
+                              </div>
+                        </div>
+                    </div>
+                    <div className='first'>
+                    <div className='top'>
+                           <img className='top-img' src={lesson4}></img>
+                            <div className='top-text'>
+                            <h4>Как ставить о оценивать задачи</h4>
+                            <p className='top-p' >   Мы ориентируемся на эргономику и ты где работаешь. Это всего лишь нажатие клавиши.</p>
+                            <div className='topic1' >
+                                        <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                         <p>22ч 30мин</p>
+                               
+                                         <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                    <p>22ч 30мин</p>
+                              
+                               
+                                    <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                    <p>22ч 30мин</p>
+                               
+                                
+                            </div> 
+                          <button className='top-btn'>Узнать больше <RiArrowRightSLine style={{
+                                 width: '30px' ,
+                                 height: '20px'
+                            }} /> </button> 
+                              </div>
+                        </div>
+                        <div className='top'>
+                           <img className='top-img' src={lesson5}></img>
+                            <div className='top-text'>
+                            <h4>Как ставить о оценивать задачи</h4>
+                            <p className='top-p' >   Мы ориентируемся на эргономику и ты где работаешь. Это всего лишь нажатие клавиши.</p>
+                            <div className='topic1' >
+                                        <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                         <p>22ч 30мин</p>
+                               
+                                         <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                    <p>22ч 30мин</p>
+                              
+                               
+                                    <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                    <p>22ч 30мин</p>
+                               
+                                
+                            </div> 
+                          <button className='top-btn'>Узнать больше <RiArrowRightSLine style={{
+                                 width: '30px' ,
+                                 height: '20px'
+                            }} /> </button> 
+                              </div>
+                        </div>
+                        <div className='top'>
+                           <img className='top-img' src={lesson6}></img>
+                            <div className='top-text'>
+                            <h4>Как ставить о оценивать задачи</h4>
+                            <p className='top-p' >   Мы ориентируемся на эргономику и ты где работаешь. Это всего лишь нажатие клавиши.</p>
+                            <div className='topic1' >
+                                        <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                         <p>22ч 30мин</p>
+                               
+                                         <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                    <p>22ч 30мин</p>
+                              
+                               
+                                    <RiAlarmLine  style={{
+                                          width: '46px' ,
+                                          height: '20px' ,
+                                          color : '#23A6F0'  }}   />
+                                    <p>22ч 30мин</p>
+                               
+                                
+                            </div> 
+                         <NavLink to={"/marketing1"}>  <button className='top-btn'>Узнать больше <RiArrowRightSLine style={{
+                                 width: '30px' ,
+                                 height: '20px'
+                            }} /> </button> </NavLink>
+                              </div>
+                        </div>
+                    </div>
+
+                </div>
+                <button className='slider-btn'>Смотреть больше</button>
+
             </div>
             
         </div>
+        <Lessons/>
+        
+       </>
+    
+
     );
 };
 
