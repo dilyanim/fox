4d7e311d79338bd5b30da6ad9db4712b069709e0
kanjar45 <kenovkenn@gmail.com>
2023-08-14 12:17:18 +0600

    abu

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import "./App.scss";
 import Header from "./components/Header";
-import Course from "./components/Course";
-import { Route, Routes } from "react-router";
+import {Route, Routes} from "react-router";
 import AboutUs from "./components/aboutUs/aboutUs";
 import Main from './components/Main/index'
 import AboutCourse from './components/AboutCourse'
+
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/aboutUs" element={<AboutUs />} />
-        <Route path="/aboutCourse" element={<AboutCourse/>} />
-      </Routes>
-      </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Main/>}/>
+                <Route path="/aboutUs" element={<AboutUs/>}/>
+                <Route path="/aboutCourse" element={<AboutCourse/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
