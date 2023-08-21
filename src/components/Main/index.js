60afa993ce07750b8d29e0fbd4ab7746ef7b101e
dilia <dilia@e[ample.com>
2023-08-22 00:59:26 +0600

    q

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -29,9 +29,9 @@ const Main = () => {
         </div>
       </div>
        <Blocks />
-      <Course />
-      <CoursesMain/>
-       <Lessons/> 
+      {/* <Course /> */}
+    {/*   <CoursesMain/>
+       <Lessons/>  */}
        {/* <Review/>  */}
     </>
   );
